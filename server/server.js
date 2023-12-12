950688c25756d6eb821c25ec175dd3912b4f3d52
ducle <lmduc.2k1@gmail.com>
2023-12-12 19:12:34 +0700

    #2 handle error

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 require("dotenv").config();
-//const dbConnect = require("./config/dbconnect");
-
-//const initRoutes = require("./routes");
+const dbConnect = require("./config/dbconnect");
+const initRoutes = require("./routes");
 //const cookieParser = require("cookie-parser");
 
 const app = express();
@@ -10,11 +9,9 @@ const app = express();
 const port = process.env.PORT || 8888;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//dbConnect();
-//initRoutes(app);
-app.use("/", (req, res) => {
-  res.send("SERVER ONNNN");
-});
+dbConnect();
+initRoutes(app);
+
 app.listen(port, () => {
   console.log("Server running on the port: " + port);
 });
